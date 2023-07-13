a5acadb4008f956eb875871c2ff928e241105d6c
changseoksong <a1373052011@gmail.com>
2023-07-13 13:35:22 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Main2 from "./sub2/src/pages/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/sub" element={<Main2 />} />
       </Routes>
     </BrowserRouter>
   );
